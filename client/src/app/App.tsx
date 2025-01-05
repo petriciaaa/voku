2602d03d1defa5063c3041b5f337e719e95106f5
Roman Kudriavtcev <petriciaaissad@mail.ru>
2025-01-05 03:24:57 +0300

    features - add tailwind

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,6 @@
 import { createUser, getAllUsers } from "@/entities/users/api/userService";
 import UserInfo from "@/entities/users/ui/UserInfo";
 import { useEffect, useState } from "react";
-
 function App() {
   const [users, setUsers] = useState<any[]>([]);
   const [email, setEmail] = useState("");
@@ -17,9 +16,9 @@ function App() {
     getAllUsers().then((data: any) => setUsers(data));
   }, [submitCount]);
   return (
-    <div className="asd">
+    <div className="asd bg-red-400">
       {users.map((user) => {
-        return <UserInfo user={user} />;
+        return <UserInfo key={user.id} user={user} />;
       })}
       <input
         type="text"
@@ -27,7 +26,16 @@ function App() {
         onChange={(e) => setEmail(e.target.value)}
       />
       {email}
-      <button onClick={handleClick}>Create</button>
+      <button onClick={handleClick} className="bg-blue-600">
+        Create
+      </button>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-3xl font-bold text-gray-900">
+            Welcome to the App
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
