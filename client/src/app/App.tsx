49743d27dbb9839d48d52fff00807b953c0eee98
Roman Kudriavtcev <petriciaaissad@mail.ru>
2025-01-05 04:16:43 +0300

    features - dev

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,11 +2,16 @@ import { createUser, getAllUsers } from "@/entities/users/api/userService";
 import UserInfo from "@/entities/users/ui/UserInfo";
 import { useEffect, useState } from "react";
 import { Button, Checkbox } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { increment } from "./store/counterSlice";
 
 function App() {
   const [users, setUsers] = useState<any[]>([]);
   const [email, setEmail] = useState("");
   const [submitCount, setSubmitCount] = useState(0);
+
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch<any>();
 
   const handleClick = async () => {
     console.log("123123123");
@@ -19,25 +24,8 @@ function App() {
   }, [submitCount]);
   return (
     <div className="asd bg-red-400">
-      {users.map((user) => {
-        return <UserInfo key={user.id} user={user} />;
-      })}
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      {email}
-      <button onClick={handleClick} className="bg-blue-600">
-        Create
-      </button>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h1 className="text-3xl font-bold text-gray-900">
-            Welcome to the App
-          </h1>
-        </div>
-      </div>
+      {count}
+      <Button onClick={() => dispatch(increment())}>sss</Button>
     </div>
   );
 }
--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
+import { store } from "./store";
+import { Provider } from "react-redux";
 import "./sass/tailwind.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
