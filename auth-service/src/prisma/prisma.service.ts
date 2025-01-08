d98a0eab9f1bf6791a8b14369bc74ed03dc41ad7
petriciaaa <140983051+petriciaaa@users.noreply.github.com>
2025-01-08 03:37:14 +0300

    Features (#10)
    * features - add tailwind
    
    * features - add mui
    
    * features - dev
    
    * features - add auth service
    
    * dev
    
    * features - auth service

---
+++ auth-service/src/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from 'prisma/generated';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      console.log('Successfully connected to the database');
+    } catch (error) {
+      console.error('Failed to connect to the database:', error);
+      throw error;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
